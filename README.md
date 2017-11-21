1b7c3523c61b7cee1ae4c17edbb569d8e8891474
alice <rainingilove@gmail.com>
2017-11-21 10:18:44 +0100

    mock tests

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# timed.jl
+# Timed.jl
 
 [![Build Status](https://travis-ci.org/aliciawyy/timed.jl.svg?branch=master)](https://travis-ci.org/aliciawyy/timed.jl)
 
--- test/test_stats.jl
+++ test/test_stats.jl
@@ -2,5 +2,5 @@ using Base.Test
 using Timed
 
 function test_dist1()
-    @test_approx_eq dist1([1, 2, 3], [1, 2, 3.]) == 14.
+    @test_approx_eq dist1([1, 2, 3], [1, 2, 3.]) == 1.
 end
