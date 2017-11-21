8308bd1c2b3bc8af7240228c0e997d8931c8a1f2
alice <rainingilove@gmail.com>
2017-11-21 22:46:44 +0100

    improve get_value

--- src/data.jl
+++ src/data.jl
@@ -4,8 +4,24 @@ struct TimedData
     timestamp::DateTime
     keys::Vector
     values::Vector
+
+    function TimedData(timestamp::DateTime, keys::Vector, values::Vector)
+        if size(keys) != size(values)
+            msg = "keys=$keys and values=$values should have the same size!"
+            throw(ArgumentError(msg))
+        end
+        new(timestamp, keys, values)
+    end
 end
 
 function get_value(d::TimedData, index::Symbol)
     return d.values[d.keys .== index][1]
+end
+
+function get_value(d::TimedData, index::Int)
+    return d.values[index]
+end
+
+function get_value(d::TimedData, index::Vector)
+    return map(i -> get_value(d, i), index)
 end
--- test/test_data.jl
+++ test/test_data.jl
@@ -4,8 +4,17 @@
     prices = [84.50, 248.03, 38.20]
     timed_data = TimedData(timestamp, tickers, prices)
 
+    @test_throws ArgumentError TimedData(timestamp, tickers, [100.10, 200.02])
     @test timed_data.timestamp == timestamp
     @test timed_data.keys == tickers
     @test timed_data.values == prices
-    @test get_value(timed_data, :SPY) == 248.03
+
+    for i = 1:3
+        expected = prices[i]
+        @test get_value(timed_data, i) == expected
+        @test get_value(timed_data, tickers[i]) == expected
+    end
+
+    @test get_value(timed_data, [:SPY, :EWJ]) == [248.03, 38.20]
+    @test get_value(timed_data, [1, 3]) == [84.50, 38.20]
 end
